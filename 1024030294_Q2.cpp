a39ec3f0f29c69a6ab5bc2e9251ba8fff38c273a
DivayJain <djain_be24@thapar.edu>
2025-12-03 01:53:57 +0530

    Create 1024030294_Q2.cpp

---
+++ 1024030294_Q2.cpp
@@ -0,0 +1,26 @@
+//student_id 1024030294
+//Assingment 2 Q2 submitted
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s; 
+    cin >> s;
+    int n = s.size(), ok = 0;
+
+    for (int len = 1; len < n; len++) {
+        string sub = s.substr(0, len);
+
+        for (int i = 1; i + len <= n; i++) {
+            if (s.substr(i, len) == sub) {
+                ok = 1;
+                break;
+            }
+        }
+
+        if (ok) break;
+    }
+
+    cout << (ok ? "YES" : "NO");
+}
